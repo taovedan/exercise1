771327730958689638fb642f1777f8a173caaa8e
Tao Ve Dan <dan@LAPTOP-3H9EQFQJ>
2023-07-20 23:52:31 +0700

    add script2

---
+++ script_2.sh
@@ -0,0 +1,17 @@
+mkdir app
+cd app
+mkdir blueprints static templates
+touch app.py .gitignore
+cd blueprints
+touch __init__.py
+mkdir home_page
+cd ..
+cd static
+touch styles.css index.js
+cd ..
+cd templates
+touch base.html home.html
+cd ..
+cd blueprints
+cd home_page
+touch __init__.py blueprint.py
